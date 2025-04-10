dc54f2724a1e4e59f53f455eba2424008f0a84d7
joe <jb5@vt.edu>
2025-04-09 21:41:33 -0400

    merge goat

--- isaac_ros-dev/src/control/src/control.cpp
+++ isaac_ros-dev/src/control/src/control.cpp
@@ -5,7 +5,8 @@
 #include "motor_controller.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "autonav_interfaces/msg/encoders.hpp" 
+#include "autonav_interfaces/msg/encoders.hpp"
+#include "autonav_interfaces/msg/gps_data.hpp"  
 #include <queue>
 #include <iostream>
 
@@ -18,11 +19,6 @@ class ControlNode : public rclcpp::Node {
     ControlNode() 
       : Node("control_node")
        {
-        
-        /*encoder_timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(100),
-            std::bind(&ControlNode::publish_encoder_data, this)
-        );*/
 
         initialize_serial_connections();
 
@@ -38,6 +34,19 @@ class ControlNode : public rclcpp::Node {
             std::bind(&ControlNode::publish_encoder_data, this)
         );
         */
+        encoder_timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100),
+            std::bind(&ControlNode::publish_encoder_data, this)
+        );
+
+        //GPS PUB
+        gpsPub = this->create_publisher<autonav_interfaces::msg::GpsData>("gps_topic", 10);
+
+        gps_timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100),
+            std::bind(&ControlNode::publish_gps_data, this)
+        );
+        
         //PATH PLANNING SUB
         /*pathPlanningSub = this->create_subscription<geometry_msgs::msg::Twist>(
             "cmd_vel", 10, std::bind(&ControlNode::path_planning_callback, this, std::placeholders::_1));
@@ -102,6 +111,7 @@ class ControlNode : public rclcpp::Node {
         }
         else{
             //TODO: logic for checking if B button is pressed 
+            
         }
         
     }
@@ -110,6 +120,8 @@ class ControlNode : public rclcpp::Node {
        // autonav_interfaces::msg::Encoders encoder_msg;
         encoder_msg.left_motor_rpm = std::to_string(motors.getLeftRPM() / 20);
         encoder_msg.right_motor_rpm = std::to_string(motors.getRightRPM() / 20);
+        encoder_msg.left_motor_count = motors.getLeftEncoderCount();
+        encoder_msg.right_motor_count = motors.getRightEncoderCount();
 
         std::string arduinoRPMs = "L:";
         arduinoRPMs += encoder_msg.left_motor_rpm;
@@ -121,8 +133,35 @@ class ControlNode : public rclcpp::Node {
         navigationEncoderPub->publish(encoder_msg);
     }*/
 
+    void publish_gps_data() {
+        autonav_interfaces::msg::GpsData gps_msg;
+        char gpsBuffer[1024] = {};
+        gpsSerial.readString(gpsBuffer, '\n', 1023, 1000);
+
+
+        std::string message(gpsBuffer);
+        std::istringstream iss(message);
+        std::string word;
+        std::vector<std::string> tokens;
+
+        while (iss >> word) {
+            tokens.push_back(word);
+        }
+        if (tokens.size() == 22) {
+
+            //std::cout << gpsBuffer << std::endl;
+            gps_msg.latitude = tokens[3];
+            gps_msg.longitude = tokens[4];
+
+            gpsPub->publish(gps_msg);
+        }
+    }
+
+    rclcpp::Publisher<autonav_interfaces::msg::GpsData>::SharedPtr gpsPub;
+    rclcpp::TimerBase::SharedPtr gps_timer_;
+
     rclcpp::Publisher<autonav_interfaces::msg::Encoders>::SharedPtr navigationEncoderPub;
-    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::TimerBase::SharedPtr encoder_timer_;
 
 
     // void path_planning_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
@@ -135,7 +174,12 @@ class ControlNode : public rclcpp::Node {
         arduinoSerial.openDevice("/dev/ttyTHS2", 9600);
         char mode[8] = "MANUAL\n";
         arduinoSerial.writeString(mode);
-        //gpsSerial.openDevice("/dev/ttyACM#", 115200);                        
+
+	
+        gpsSerial.openDevice("/dev/ttyTCU0", 115200);
+        char gpsStartCmd[32] = "log bestposa ontime 2\r\n";
+        gpsSerial.writeString("unlogall\r\n");
+        gpsSerial.writeString(gpsStartCmd);                        
     }
 
 };
