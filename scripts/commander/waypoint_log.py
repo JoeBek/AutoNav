c26770e5df6f922ed387ab92df4638d450690ae8
joe <jb5@vt.edu>
2025-05-22 14:45:01 -0400

    how many software engineers does it take to configure a lightbulb?

---
+++ scripts/commander/waypoint_log.py
@@ -0,0 +1,129 @@
+import rclpy
+from nav2_simple_commander.robot_navigator import BasicNavigator
+import yaml
+from ament_index_python.packages import get_package_share_directory
+import os
+import sys
+import time
+
+
+import math
+from geographic_msgs.msg import GeoPose
+from geometry_msgs.msg import Quaternion
+
+
+'''
+from https://docs.nav2.org/tutorials/docs/navigation2_with_gps.html
+
+needs config file.. I set it up in slam so this doesnt need its own package. Add the waypoints to that and test
+
+'''
+def quaternion_from_euler(roll, pitch, yaw):
+    """
+    Converts euler roll, pitch, yaw to quaternion
+    """
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+
+    q = Quaternion()
+    q.w = cy * cp * cr + sy * sp * sr
+    q.x = cy * cp * sr - sy * sp * cr
+    q.y = sy * cp * sr + cy * sp * cr
+    q.z = sy * cp * cr - cy * sp * sr
+    return q
+
+
+def euler_from_quaternion(q: Quaternion):
+    """
+    Convert a quaternion into euler angles
+    taken from: https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+    """
+    t0 = +2.0 * (q.w * q.x + q.y * q.z)
+    t1 = +1.0 - 2.0 * (q.x * q.x + q.y * q.y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (q.w * q.y - q.z * q.x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (q.w * q.z + q.x * q.y)
+    t4 = +1.0 - 2.0 * (q.y * q.y + q.z * q.z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z
+
+
+def latLonYaw2Geopose(latitude: float, longitude: float, yaw: float = 0.0) -> GeoPose:
+    """
+    Creates a geographic_msgs/msg/GeoPose object from latitude, longitude and yaw
+    """
+    geopose = GeoPose()
+    geopose.position.latitude = latitude
+    geopose.position.longitude = longitude
+    geopose.orientation = quaternion_from_euler(0.0, 0.0, yaw)
+    return geopose
+
+
+class YamlWaypointParser:
+    """
+    Parse a set of gps waypoints from a yaml file
+    """
+
+    def __init__(self, wps_file_path: str) -> None:
+        with open(wps_file_path, 'r') as wps_file:
+            self.wps_dict = yaml.safe_load(wps_file)
+
+    def get_wps(self):
+        """
+        Get an array of geographic_msgs/msg/GeoPose objects from the yaml file
+        """
+        gepose_wps = []
+        for wp in self.wps_dict["waypoints"]:
+            latitude, longitude, yaw = wp["latitude"], wp["longitude"], wp["yaw"]
+            gepose_wps.append(latLonYaw2Geopose(latitude, longitude, yaw))
+        return gepose_wps
+
+
+class GpsWpCommander():
+    """
+    Class to use nav2 gps waypoint follower to follow a set of waypoints logged in a yaml file
+    """
+
+    def __init__(self, wps_file_path):
+        self.navigator = BasicNavigator("basic_navigator")
+        self.wp_parser = YamlWaypointParser(wps_file_path)
+
+    def start_wpf(self):
+        """
+        Function to start the waypoint following
+        """
+        self.navigator.waitUntilNav2Active(localizer='robot_localization')
+        wps = self.wp_parser.get_wps()
+        self.navigator.followGpsWaypoints(wps)
+        while (not self.navigator.isTaskComplete()):
+            time.sleep(0.1)
+        print("wps completed successfully")
+
+
+def main():
+    rclpy.init()
+
+    # allow to pass the waypoints file as an argument
+    default_yaml_file_path = os.path.join(get_package_share_directory(
+        "slam"), "config", "waypoints.yaml")
+    if len(sys.argv) > 1:
+        yaml_file_path = sys.argv[1]
+    else:
+        yaml_file_path = default_yaml_file_path
+
+    gps_wpf = GpsWpCommander(yaml_file_path)
+    gps_wpf.start_wpf()
+
+
+if __name__ == "__main__":
+    main()
